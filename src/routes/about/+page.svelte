8c8433f8c145a08ee3b441198e8591c205c3fc44
BogGoro <151636316+BogGoro@users.noreply.github.com>
2024-07-09 21:32:01 +0300

    add about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,22 +5,37 @@
 
 <div class="text-column">
 	<h1>About this app</h1>
-
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		Meet up is a user-friendly platform designed to help you explore upcoming events, manage your own activities, and connect with others in your community.
 	</p>
-
-	<pre>npm create svelte@latest</pre>
-
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		Here's what you can expect:
 	</p>
-
 	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
+		<ul>
+			<li>Home: Our central hub provides a clear and concise overview of what's happening. You might find featured events, popular categories, or upcoming activities based on your interests.</li>
+			<li>About Us: Get to know us better! This page delves into the purpose of Meet up, our mission, and the team behind it. You can also learn more about how the platform works and its benefits.</li>
+			<li>Events: This is the heart of the action! Discover a wide range of events happening around you. You can browse by category, date, or search for specific keywords. Whether you're interested in workshops, concerts, social gatherings, or volunteer opportunities, there's something for everyone.</li>
+		</ul>
+	
+	
+	<p>
+		Take Charge of Your Schedule:
+	</p>
+	<ul>
+		<li>Create Events: Planning your own event? Share it with the community! Our user-friendly interface allows you to easily create event listings, complete with details like date, time, location, and a captivating description.</li>
+		<li>Manage Your Events: Keep your events organized and up-to-date. Edit existing events, add photos, or update any information as needed.</li>
+		<li>Sign Up for Events: Found something interesting? With a few clicks, you can register for any event that sparks your curiosity. Stay informed about event updates and connect with other attendees.</li>
+	</ul>
+	
+	
+	<p>
+		Join the Community:
+	</p>
+	<p>
+		Meet up is more than just a calendar – it's a platform for fostering connections. Sign up for a free account to unlock the full potential of the site.
+	</p>
+	<p>
+		We look forward to welcoming you and helping you navigate the exciting world of events happening in your community!
 	</p>
 </div>
