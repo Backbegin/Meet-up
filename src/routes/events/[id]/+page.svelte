6889374076c4ad3d30c72f7f50287566a88a47f3
BogGoro <151636316+BogGoro@users.noreply.github.com>
2024-07-13 19:38:29 +0300

    Fix null problem

--- src/routes/events/[id]/+page.svelte
+++ src/routes/events/[id]/+page.svelte
@@ -2,8 +2,8 @@
 	import type { Event } from '$lib/types/event';
 
 	interface MyDataType {
-		error: string;
-		event: Event;
+		error: string | null;
+		event: Event | null;
 	}
 
 	export let data: MyDataType;
@@ -13,16 +13,16 @@
 </script>
 
 <svelte:head>
-	<title>{event.title}</title>
-	<meta name="description" content={event.description} />
+	<title>{event?.title || 'Event not found'}</title>
+	<meta name="description" content={event?.description || 'Event does not exist in the database'} />
 </svelte:head>
 
 {#if error !== null}
 	<h1>{error}</h1>
 {:else}
 	<h1>Here will be single event page</h1>
-	<a href="/events/{event.id}/edit">
+	<a href="/events/{event?.id}/edit">
 		<button> Edit event </button>
 	</a>
-	<div>{event.title} - {event.description} - {event.place} - {event.date} - {event.time}</div>
+	<div>{event?.title} - {event?.description} - {event?.place} - {event?.date} - {event?.time}</div>
 {/if}
